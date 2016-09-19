6e9a7c0b0332a0b3f4b3039de2326aacb6cc16f3
SliinQ <sliinq2012@gmail.com>
2016-09-19 23:26:58 +0200

    Finally added color support, also a couple of extra modules.

--- bar.py
+++ bar.py
@@ -4,11 +4,22 @@ import blocks
 
 
 class Bar:
-    def __init__(self, delay=5):
+    def __init__(self, delay=1, foreground='#FFFFFF', background='#000000',
+                 xresources=False):
         self.__delay = delay
+        if xresources:
+            # TODO: Load xResources
+            pass
+        else:
+            self.__foreground = foreground
+            self.__background = background
         self.blocks = []
 
     def add_block(self, block):
+        # if hasattr(block, 'foreground') and not block.foreground:
+        #     block.foreground = self.__foreground
+        # if hasattr(block, 'background') and not block.background:
+        #     block.background = self.__background
         self.blocks.append(block)
 
     def add_blocks(self, blocks):
@@ -16,8 +27,6 @@ class Bar:
             self.add_block(block)
 
     def render(self):
-        # for block in self.blocks:
-        #     block.update()
         print(''.join([b() for b in self.blocks]))
         stdout.flush()
 
@@ -28,22 +37,27 @@ class Bar:
 
 
 def main():
-    bar = Bar()
+    bar = Bar(delay=5)
 
     bar.add_blocks([
             blocks.Align('l'),
-            blocks.Workspaces(monitor='DVI-D-0'),
-            blocks.Static(text='Notice the fog.', padding=(5, 2)),
+            blocks.Workspaces(monitor='DVI-D-0',
+                              padding=(2, 5)),
+            blocks.Static(text='Notice the fog.'),
             blocks.Volume(interval=1),
 
             blocks.Align('c'),
-            blocks.Clock(),
+            blocks.Clock(swap=True),
 
             blocks.Align('r'),
             blocks.Weather(location='haren,gn',
                            apikey='7c8ca2cd7dcab68d7f9e4d08a2a08595',
-                           padding=(0, 5)),
-            blocks.Workspaces(monitor='DVI-I-0')
+                           append='°C'),
+            blocks.Memory(append='M'),
+            blocks.IPAddress(interface='enp3s0'),
+            blocks.Ping(append='ms', host='vps298888.ovh.net'),
+            blocks.Workspaces(monitor='DVI-I-0',
+                              padding=(5, 2))
     ])
 
     try:
--- blocks.py
+++ blocks.py
@@ -1,24 +1,43 @@
-from datetime import datetime
-from time import time
 from alsaaudio import Mixer
+from datetime import datetime
+import fcntl
 import i3ipc
-from urllib.request import urlopen
 import json
+import re
+import socket
+import struct
+import subprocess
+from time import time
+from urllib.request import urlopen
 
 
 class Base:
     i3 = i3ipc.Connection()
 
-    def __init__(self, icon='', interval=1, padding=0):
+    def __init__(self, icon='', interval=1, padding=1, prepend='', append='',
+                 foreground=None, background=None, swap=False):
         self.__icon = icon
-        # self.__template = '%{{{align}}}%{{{foreground}}}%{{{background}}}{value}%{{F-}}%{{B-}}'
+
+        self.__template = '{lpadding}{prepend}{value}{append}{rpadding}'
+        self.__fg = lambda o: '%{{F{foreground}}}' + o + '%{{F-}}'
+        self.__bg = lambda o: '%{{B{background}}}' + o + '%{{B-}}'
+        self.__swap = lambda o: '%{{R}}' + o + '%{{R}}'
+
         self.__interval = interval
         self.__prevtime = 0
         self.__padding = padding
+        self.__append = append
+        self.__prepend = prepend
+
+        self.foreground = foreground
+        self.background = background
+        self.swap = swap
+
         self.output = ''
 
     def __call__(self):
         if self.__interval == -1:
+            # Only call update once
             if self.__prevtime != -1:
                 self.update()
                 self.__prevtime = -1
@@ -27,14 +46,28 @@ class Base:
             self.__prevtime = time()
 
         conf = dict(
+                    foreground=self.foreground,
+                    background=self.background,
+
                     lpadding=' ' * (self.__padding[0] if type(self.__padding)
                                     == tuple else self.__padding),
                     rpadding=' ' * (self.__padding[1] if type(self.__padding)
                                     == tuple else self.__padding),
                     icon=self.__icon,
-                    value=self.output
-                )
-        return '{lpadding}{value}{rpadding}'.format_map(conf)
+                    value=self.output,
+                    append=self.__append,
+                    prepend=self.__prepend
+        )
+
+        output = self.__template
+        if self.foreground:
+            output = self.__fg(output)
+        if self.background:
+            output = self.__bg(output)
+        if self.swap:
+            output = self.__swap(output)
+
+        return output.format_map(conf)
 
     def update(self):
         raise NotImplementedError('Function needs to be implemented')
@@ -117,3 +150,57 @@ class Static(Base):
 
     def update(self):
         self.output = self.text
+
+
+class Memory(Base):
+    def __init__(self, percentage=False, interval=5, **kwds):
+        super().__init__(interval=interval, **kwds)
+        self.percentage = percentage
+
+    def update(self):
+        with open('/proc/meminfo', 'r') as mem:
+            total = 0
+            available = 0
+            for line in mem:
+                line_split = line.split()
+                if line_split[0] == 'MemTotal:':
+                    total = int(line_split[1])
+                elif line_split[0] in ['MemFree:', 'Buffers:', 'Cached:']:
+                    available += int(line_split[1])
+        used_mb = round((total-available)/1024)
+        used_perc = round((available/total)*100)
+        self.output = used_perc if self.percentage else used_mb
+
+
+class IPAddress(Base):
+    def __init__(self, interface='eth0', interval=900, **kwds):
+        super().__init__(interval=interval, **kwds)
+        self.interface = interface.encode('utf-8')
+
+    def update(self):
+        def get_ip_address(ifname):
+            # Props to 'Martin Konecny' on SO
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            return socket.inet_ntoa(fcntl.ioctl(
+                s.fileno(),
+                0x8915,  # SIOCGIFADDR
+                struct.pack('256s', ifname[:15])
+            )[20:24])
+        self.output = get_ip_address(self.interface)
+
+
+class Ping(Base):
+    def __init__(self, host='8.8.8.8', interval=5, **kwds):
+        super().__init__(interval=interval, **kwds)
+        self.host = host
+
+    def update(self):
+        ping = subprocess.Popen('ping -c1 -W1 {}'.format(self.host),
+                                shell=True, stdout=subprocess.PIPE
+                                ).stdout.read()
+        reg = re.search('\d\d\.\d{3}/(\d\d\.\d{3})/\d\d\.\d{3}',
+                        ping.decode())
+        if reg:
+            self.output = round(float(reg.groups()[0]))
+        else:
+            self.output = 0
